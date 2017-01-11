ff341e904a72daf503a3a7e029665b09999fb08c
Alena <alena13lyahova@gmail.com>
2017-01-11 16:44:57 +0200

    Использовали инкапсуляцию

--- snake-full/snake-full/Point.cs
+++ snake-full/snake-full/Point.cs
@@ -12,6 +12,18 @@ namespace snake_full
         public int y;
         public char sim;
 
+        public Point()
+        {
+
+        }
+
+        public Point(int _x, int _y, char _sim)
+        {
+            x = _x;
+            y = _y;
+            sim = _sim;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
