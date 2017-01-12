ac9daf3ae9d817961893c674698db4b318cdc7a5
Alena <alena13lyahova@gmail.com>
2017-01-12 16:04:36 +0200

    Add class Snake

--- snake-full/snake-full/Point.cs
+++ snake-full/snake-full/Point.cs
@@ -17,6 +17,38 @@ namespace snake_full
 
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sim = p.sim;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if (direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+            else if (direction == Direction.DOWN)
+            {
+                y = y - offset;
+            }
+        }
+
+        public override string ToString()
+        {
+            return x + "," + y + "," + sim;
+        }
+
         public Point(int _x, int _y, char _sim)
         {
             x = _x;
